a9e5c22c6a0e3db13d677d363743d56235d54cca
Mitul Agrawal <mitulagr2@gmail.com>
2025-06-06 01:51:43 +0530

    update website copy

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,64 +2,78 @@
     import Card from '../components/Card.svelte';
 </script>
 
-<div class="max-w-svw min-h-screen p-2 grid gap-2 lg:grid-cols-3 lg:auto-rows-fr">
+<div class="grid min-h-screen max-w-svw gap-2 p-2 lg:auto-rows-fr lg:grid-cols-3">
     <Card>
-            <img src="logo.png" alt="CloakMind logo" class="h-64">
-            <h1 class="eagle-lake text-5xl mt-2">CloakMind</h1>
+        <img src="favicon.png" alt="CloakMind logo" class="h-64" />
+        <h1 class="eagle-lake mt-2 text-5xl">CloakMind</h1>
     </Card>
-    
+
     <Card>
         <span>
             A tiny non-corporate run by
             <!-- two Product Engineers:  -->
-            <a href='https://shivangagr.netlify.app' target="_blank" class="eagle-lake accent link">
+            <a
+                href="https://shivangagr.netlify.app/?ref=cloakmind"
+                target="_blank"
+                class="eagle-lake accent link"
+            >
                 Shivang
             </a>, and
-            <a href='https://mitulagr.vercel.app' target="_blank" class="eagle-lake accent link">
+            <a
+                href="https://mitulagr2.vercel.app/?ref=cloakmind"
+                target="_blank"
+                class="eagle-lake accent link"
+            >
                 Mitul
             </a>
         </span>
     </Card>
-    
+
     <Card>
-        <span class="font-bold text-xl">
+        <img src="pawss.png" alt="Pawss logo" class="m-4 h-24" />
+        <span class="max-w-sm">
+            Meet
+            <!-- <span class="font-bold text-transparent bg-gradient-to-r from-[#FCD400] to-[#C59A34] bg-clip-text"> -->
+            <span class="font-bold"> Pawss </span>, the computer agent that sees
+            <br /> what you see and works where you work.
+        </span>
+        <a
+            href="https://pawss.party"
+            target="_blank"
+            class="mt-4 rounded-full border-2 p-1 pr-4 pl-4 text-sm font-semibold"
+        >
+            Learn more
+        </a>
+    </Card>
+
+    <Card>
+        <span class="text-xl font-bold">
             For the builders. By the builders.
             <!-- We ask questions. -->
             <!-- We ask questions, we build, we break, we learn, we share. -->
         </span>
     </Card>
-    
+
     <Card>
-        <img src="deepseekret.svg" alt="DeepSeekret logo" class="h-24 m-4">
-        <span class="max-w-sm">Keep your chats a
-            <!-- <span class="font-bold text-transparent bg-gradient-to-r from-[#FCD400] to-[#C59A34] bg-clip-text"> -->
-            <span class="font-bold">
-                DeepSeekret
-            </span>.
-            <br />
-            Secure, open-source, and free.
+        <img src="aetheriumai.png" alt="DeepSeekret logo" class="m-4 h-24" />
+        <span class="font-bold">AetheriumAI</span>
+        <span class="mt-2 max-w-sm rounded-full text-sm">
+            There's more to your PDFs than <br /> meets the eye. Let AI show you.
         </span>
-        <a href='https://cloakmind.github.io/deepseekret' target="_blank" class="mt-4 p-1 pl-4 pr-4 text-sm font-semibold border-2 rounded-full">
+        <a
+            href="http://aetheriumai.vercel.app/?ref=cloakmind"
+            target="_blank"
+            class="mt-4 rounded-full border-2 p-1 pr-4 pl-4 text-sm font-semibold"
+        >
             Learn more
         </a>
     </Card>
-    
-    <Card>
-        <img src="logo.png" alt="DeepSeekret logo" class="h-24 m-4">
-        <span>AgentCrest</span>
-        <span class="mt-2 text-sm text-[#999] rounded-full">Coming soon</span>
-    </Card>
-    
+
     <Card>
         <span class="max-w-md text-sm">
-            If you’re enjoying Daily,
-            <a href='https://github.com/CloakMind' class="link">follow us</a> to be first to know what’s next.
-            This site runs on Svelte.
-            <a href='https://cloakmind.github.io' class="link">Learn how</a> to make one too.
+            We build for the love of building. Drop us an
+            <a href="mailto:cloakmind@gmail.com" class="link">email</a> if we <br /> can build together, or
+            talk about tech or life as builders.
         </span>
     </Card>
 </div>
-
-<style>
-    /* .deepseekret { color: #FCD400; } */
-</style>
